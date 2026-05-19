df12290fa9ae4c58c8f25342180ff1773c463f02
MartCabr <mrtc7878@gmail.com>
2026-05-19 17:35:16 -0400

    Separación de arquitectura: HTML, CSS y JS
    Se reestructuro y "dividio" todo el codigo; se creó un html para cada pagina, el css ahora tiene un archivo independiente al igual que javascript, y se conectó cada html al javascript, dejando la base lista para lo que viene

---
+++ Evaluacion/assets/js/main.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("JavaScript vinculado correctamente");
+    
+    inicializarProyecto();
+});
+
+ // Función inicializadora para pruebas de estructura
+function inicializarProyecto() {
+    
+}
